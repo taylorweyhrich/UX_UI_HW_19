10d624a792ad61d70f9f82bfc1a8a051264b4080
taylorweyhrich <taylorweyhrich@gmail.com>
2023-04-17 17:28:07 -0700

    added ui

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+console.log("Your index.j file is loaded correctly!");
+
+$("button").hover(function(){
+    $(this).css("background-color", "#90AEB2");
+    }, function(){
+    $(this).css("background-color", "#798071");
+  });
